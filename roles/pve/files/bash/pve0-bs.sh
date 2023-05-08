eb594b9a5e4cabd5260e32ba8f225fe144a39a4d
EZecarias <120346104+EZecarias@users.noreply.github.com>
2023-05-08 10:30:30 -0400

    Update pve0-bs.sh

--- roles/pve/files/bash/pve0-bs.sh
+++ roles/pve/files/bash/pve0-bs.sh
@@ -2,8 +2,8 @@
 
 #update apt and install syncthing
 curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | tee /etc/apt/>
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | tee /etc/a>
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
 apt-get update -y
 apt-get install syncthing -y
 wget 192.168.0.101:8080/0syncthing@.service -P /etc/systemd/system/syncthing@.service
