da5d3351f6b9b166c2635813616f69716b95caa3
EZecarias <120346104+EZecarias@users.noreply.github.com>
2023-05-08 07:18:33 -0400

    Update pve0-install.sh

--- roles/pve/files/bash/pve0-install.sh
+++ roles/pve/files/bash/pve0-install.sh
@@ -1,2 +1,2 @@
-wget 192.168.0.101:8080/pve0_driver-nvidia.conf --directory-prefix=/usr/share/X11/xorg.conf.d/driver-nvidia.conf
+wget 192.168.0.101:8080/pve0_driver-nvidia.conf -P /usr/share/X11/xorg.conf.d/driver-nvidia.conf
 xinit -- -dpi 96 >/dev/tty2 2>&1
